0daff66e6fb67e595deb5b5b41b6296559c74fc5
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-21 00:03:32 +0900

    Feat(P2): 중복되는 문자 찾기 및 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
+    /* 중복되는 문자열 찾기 */
+    private static String duplicateString(String cryptogram) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+
+        for (Character c :cryptogram.toCharArray()) {
+            duplicateRemove(st,c);
+        }
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+
+    /* 중복되는 문자열 제거 */
+    private static void duplicateRemove(Stack<Character> st,Character c){
+        if(st.isEmpty()){
+            st.push(c);
+            return;
+        }
+        if(st.peek() == c){
+            st.pop();
+            return;
+        }
+        st.push(c);
+    }
+
 }
