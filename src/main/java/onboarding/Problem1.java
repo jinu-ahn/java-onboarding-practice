d481c07c9c659cff9db5b2b090ebbc4d0b3e3315
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-20 22:55:59 +0900

    Feat: Exception 상황 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,4 +17,39 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    /* 펼친 페이지가 첫페이지 또는 끝페이지인지 확인 */
+    private static void firstOrMaxPageException(List<Integer> page) {
+        for (Integer i : page) {
+            if(i == FIRST_PAGE || i == MAX_PAGE)
+                exception = true;
+        }
+    }
+
+    /* 포비와 크롱의 리스트의 값이 2인지 확인 */
+    private static void sizeValidator(int pageSize) {
+        if(pageSize != LIST_SIZE)
+            exception = true;
+    }
+
+    /* 왼쪽페이지가 홀수인지 확인 */
+    private static void leftPageValidation(int leftPage){
+        if(leftPage/2 == 1)
+            exception = true;
+    }
+
+    /* 오른쪽페이지가 짝수인지 확인 */
+    private static void rightPageValidation(int rightPage) {
+        if(rightPage /2 == 0)
+            exception = true;
+    }
+
+    /* 연속되는 페이지 인지 확인 */
+    private static void continuityPage(List<Integer> page){
+        int left = page.get(0);
+        int right = page.get(1);
+
+        if(right - left != 1)
+            exception = true;
+    }
 }
