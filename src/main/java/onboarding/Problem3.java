1815318c834af87eaddf8648e165d23dd3bcf723
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-21 00:23:57 +0900

    Feat(P3): 3,6,9가 포함된다면 손뼉 치기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,14 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /* 손뼉 횟수 계산 */
+    private static int clap(int number, int clapCount){
+        String cNumber = String.valueOf(number);
+        for (char c : cNumber.toCharArray()) {
+            if(c == '3' || c == '6' || c == '9')
+                clapCount++;
+        }
+        return clapCount;
+    }
 }
