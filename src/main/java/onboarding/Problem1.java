eddcabf06735704416ac7d5cfe8ed683469bbec9
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-20 22:54:40 +0900

    Feat: 상수 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static final int FIRST_PAGE = 0;
+    static final int MAX_PAGE = 400;
+    static final int POBI_WIN = 1;
+    static final int CRONG_WIN = 2;
+    static final int DRAW = 0;
+    static final int EXCEPTIONS_SITUATION = -1;
+    static final int LIST_SIZE = 2;
+
+    static boolean exception = false;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
