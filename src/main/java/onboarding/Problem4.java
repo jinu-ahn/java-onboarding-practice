04834a0dbcf3a98bbc6b7c428d85a6c28170570b
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-21 23:26:59 +0900

    Feat(P4): 상수 선언

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+    static final char LOWER_FIRST_CASE = 'a';
+    static final char LOWER_LAST_CASE = 'z';
+    static final char UPPER_FIRST_CASE = 'A';
+    static final char UPPER_LAST_CASE = 'Z';
     public static String solution(String word) {
         String answer = "";
         return answer;
