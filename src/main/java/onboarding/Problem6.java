ddff85d2fe12d312182560eab66431a5af6a5a4f
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-22 00:57:08 +0900

    Feat(P6): 상수 선언

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+    static final int EMAIL_MIN_LENGTH = 11;
+    static final int EMAIL_MAX_LENGTH = 20;
+    static final int NICKNAME_MIN_LENGTH = 1;
+    static final int NICKNAME_MAX_LENGTH = 20;
+    static final String DOMAIN = "email.com";
+    static HashMap<String,Integer> map = new HashMap<>();
+    static List<String> answer = new ArrayList<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
