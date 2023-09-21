86267bd3d4199aae0547cd169f796e9b60a982b8
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-21 23:55:13 +0900

    Feat(P5): 상수 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,21 @@
 package onboarding;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static final int FIFTY_THOUSAND = 50000;
+    static final int TEN_THOUSAND = 10000;
+    static final int FIVE_THOUSAND = 5000;
+    static final int ONE_THOUSAND = 1000;
+    static final int FIVE_HUNDRED = 500;
+    static final int ONE_HUNDRED = 100;
+    static final int FIFTY = 50;
+    static final int TEN = 10;
+    static final int ONE = 1;
+
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
