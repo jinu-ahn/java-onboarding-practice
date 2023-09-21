6eb998b27b339f17a430d7d87949f394f6dd5c9a
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-21 23:29:20 +0900

    Feat(P4): 알파벳이라면 청계구리 문자로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,8 +6,25 @@ public class Problem4 {
     static final char UPPER_FIRST_CASE = 'A';
     static final char UPPER_LAST_CASE = 'Z';
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : word.toCharArray())
+            sb.append(changeReverseAlpha(c));
+
+        return sb.toString();
+    }
+
+    /* 받은 매개변수를 청개구리 언어로 변경 */
+    private static char changeReverseAlpha(char alpha) {
+        if(isLowerAlpha(alpha)){
+            alpha = (char) (LOWER_FIRST_CASE + LOWER_LAST_CASE - alpha);
+            return alpha;
+        }
+        if(isUpperAlpha(alpha)){
+            alpha = (char) (UPPER_FIRST_CASE + UPPER_LAST_CASE - alpha);
+            return alpha;
+        }
+        return alpha;
     }
 
     /* 대문자 알파벳인지 확인 */
