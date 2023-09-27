153014fc15287da1ae349efb377d0b6a387ad9fa
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-09-27 22:31:39 +0900

    Feat(P7): 상수 설정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    static final int USER_LOW_VALUE = 1;
+    static final int USER_MAX_VALUE = 30;
+    static final int FRIENDS_MIN_VALUE = 1;
+    static final int FRIENDS_MAX_VALUE = 10000;
+    static final int VISITOR_MIN_VALUE = 1;
+    static final int VISITOR_MAX_VALUE = 10000;
+    static final int ID_MIN_VALUE = 1;
+    static final int ID_MAX_VALUE = 30;
+    static final String REGEXP = "^[a-z]*$";
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
